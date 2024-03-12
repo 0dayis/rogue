e4d5721ff35236fa309d0637753e5e5faa90dfe5
John Paul <nil@exploit.rs>
2024-03-13 01:17:56 +0900

    Update default hue value in setting-utils.ts

--- src/utils/setting-utils.ts
+++ src/utils/setting-utils.ts
@@ -1,5 +1,5 @@
 export function getDefaultHue(): number {
-  const fallback = '250'
+  const fallback = '300'
   const configCarrier = document.getElementById('config-carrier')
   return parseInt(configCarrier?.dataset.hue || fallback)
 }
